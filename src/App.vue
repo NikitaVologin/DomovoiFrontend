8a6380ef6c034fa9737bd1743c5f687522a14fe3
TalkativeGuy <vLaZoV_007@mail.ru>
2024-04-03 09:37:16 +0500

    init vue/vite project

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+	
+</div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import axios from 'axios'
+
+export default defineComponent({
+	components: {  },
+	data() {
+		
+	},
+	mounted() {
+		axios.get('https://agency.amberday.net/api/', {}).then(res => {
+			console.log(res)
+		})
+	},
+	computed: {
+	},
+	watch: {
+		
+	},
+	methods: {
+		
+	},
+	directives: {
+		
+	}
+})
+</script>
+
+
+<style scoped>
+/* .value {
+	color: v-bind(valColor)
+} */
+</style>
